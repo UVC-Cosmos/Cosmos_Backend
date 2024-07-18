ff5e4030b17154d53fd02ea4c84f84fc77abc1e5
leejhyun <leejh44351@naver.com>
2024-07-18 16:31:31 +0900

    [FEAT]사용자 별 권한 추가 및 권한별 접근 미들웨어 구현 관련#6

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import userService from '../services/userService.js';
+import s from '../services/userService.js';
 import passport from '../config/passport.js';
 import { isAuthenticated } from '../middlewares/index.js';
 import logger from '../libs/logger.js';
@@ -7,31 +7,37 @@ import logger from '../libs/logger.js';
 const router = express.Router();
 
 router.post('/login', (req, res, next) => {
+  console.log('auth.login');
+
+  // 이미 로그인된 사용자인지 확인
+  if (req.isAuthenticated()) {
+    return res.status(400).json({ message: '이미 로그인된 상태입니다.' });
+  }
   logger.info('auth.login.SessionInit', req.session);
 
   passport.authenticate('local', (err, user) => {
-    if(err) {
+    if (err) {
       return res.status(400).json({ message: err.message });
     }
-    if(!user) {
+    if (!user) {
       return res.status(400).json({ message: err.message });
     }
 
     req.logIn(user, (err) => {
-      if(err) {
+      if (err) {
         return next(err);
       }
       return res.status(200).json(user);
-    })
+    });
   })(req, res, next);
 });
 
 router.post('/logout', isAuthenticated, (req, res, next) => {
-  req.logout(err => {
+  req.logout((err) => {
     if (err) {
       return next(err);
     }
-    req.session.destroy(err => {
+    req.session.destroy((err) => {
       if (err) {
         return res.status(500).json({ message: 'Failed to destroy session' });
       }
--- routes/user.js
+++ routes/user.js
@@ -13,30 +13,24 @@ router.post('/', async (req, res) => {
       password: req.body.password,
       userName: req.body.userName,
       email: req.body.email,
-    }
+    };
     const result = await userService.createUser(params);
     logger.info('routes.user.post.result', result);
     res.status(200).json(result);
   } catch (err) {
     logger.error('ERROR: routes.user.POST', err);
-    if ( params.userId === undefined ){
+    if (params.userId === undefined) {
       res.status(400).json({ error: '유효하지 않은 아이디 입니다.' });
-    }
-    else if ( params.password === undefined ){
+    } else if (params.password === undefined) {
       res.status(400).json({ error: '유효하지 않은 비밀번호 입니다.' });
-    }
-    else if ( params.userName === undefined ){
+    } else if (params.userName === undefined) {
       res.status(400).json({ error: '유효하지 않은 이름 입니다.' });
-    }
-    else if ( params.email === undefined ){
+    } else if (params.email === undefined) {
       res.status(400).json({ error: '유효하지 않은 이메일 입니다.' });
     }
 
     res.status(500).json(err);
   }
 });
-
-
-
 
 export default router;
